cdb88016d3f733212d98b52e8a669f0b93465004
Soham Sahare <sohamsahare@gmail.com>
2018-09-14 16:12:24 +0530

    first implementation that passes all the test cases

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -5,9 +5,35 @@ namespace CaesarCipher
 {
     public static class RotationalCipher
     {
+        
         public static string Rotate(string text, int shiftKey)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            const int upperAlphabet = 65;
+            const int lowerAlphabet = 97;
+            // ASCII code for a -> 97 z -> 122 && A -> 65 Z -> 90
+            string cipher = "";
+            int charInAscii = 0;
+            foreach(char c in text){
+                charInAscii = c;
+                // if alphabet is a uppercase
+                if(charInAscii >= 65 && charInAscii <= 90){
+                    charInAscii = CaesarCipherHelper(charInAscii, upperAlphabet, shiftKey);
+                }
+                // if alphabet is a lowercase
+                else if(charInAscii >= 97 && charInAscii <= 122){
+                    charInAscii = CaesarCipherHelper(charInAscii, lowerAlphabet, shiftKey);
+                }
+                char cipherChar = (char)charInAscii;
+                cipher += cipherChar.ToString();
+            }
+            return cipher;
+        }
+
+        private static int CaesarCipherHelper(int charInAscii, int alphabetStart, int shiftKey){
+            charInAscii = charInAscii - alphabetStart + shiftKey;
+            charInAscii %= 26;
+            charInAscii += alphabetStart;
+            return charInAscii;
         }
     }
 }
